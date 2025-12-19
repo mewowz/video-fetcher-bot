1cfd569ba019e11d41da9f2d7992d916864aba3b
mewowz <mewowz@lainx.org>
2025-12-19 15:22:20 -0800

    Building the worker daemon; mostly a lot of reading sc for yt-dlp

---
+++ worker/worker.py
@@ -0,0 +1,23 @@
+import asyncio
+import redis
+
+from yt_dlp import YoutubeDL
+
+YTDL_OPTS_BASE = {
+        "quiet": True,
+        "no_warnings": True,
+        "format": "mp4", # Keep this as the default
+        "check_formats": True, # Check all the formats to see if any of them are downloadable, at least
+        "format": "bv*+ba",
+        "restrictfilenames": True,
+        "noplaylist": True,
+        "final_ext": "mp4",
+        "fixup": "warn",
+        "noprogress": True,
+        "consoletitle": False,
+
+
+
+}
+
+
