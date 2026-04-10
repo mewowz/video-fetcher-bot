729e63c7b0fee72ea2abcdfb779ff3075c590333
mewowz <mewowz@lainx.org>
2026-04-09 19:27:12 -0700

    Split functionality of making the download path. One function creates the path and the other simply generates the related Path object

--- worker/downloader.py
+++ worker/downloader.py
@@ -39,7 +39,7 @@ class Downloader:
     def __init__(
             self, 
             name: str, 
-            ytdlp_opts: dict, 
+            ytdlp_opts: dict = None, 
             custom_logger: logging.Logger = None, 
             downloader_opts: dict = dict()
     ):
@@ -51,12 +51,17 @@ class Downloader:
         else:
             self.logger = custom_logger
 
-        self.ytdlp_opts = (YTDL_OPTS_BASE | ytdlp_opts)
+        if isinstance(ytdlp_opts, dict):
+            self.ytdlp_opts = (YTDL_OPTS_BASE | ytdlp_opts)
+        else:
+            self.ytdlp_opts = YTDL_OPTS_BASE
         self.ytdlp_opts["logger"] = self.logger
-        self.logger.debug(f"Created Downloader '{self.name}'")
 
-        if downloader_opts.get("dl_type") in VALID_DOWNLOAD_LOCATION_TYPES:
-            self.dl_type = downloader_opts["dl_type"]
+        if isinstance(downloader_opts, dict):
+            if downloader_opts.get("dl_type") in VALID_DOWNLOAD_LOCATION_TYPES:
+                self.dl_type = downloader_opts["dl_type"]
+            else:
+                self.dl_type = "local"
 
 
         try:
@@ -64,6 +69,9 @@ class Downloader:
         except FileExistsError as e:
             self.logger.error(f"Unable to create dir for saving videos @ '{str(self.downloader_opts['local_dl_path'])}'")
             raise # for now, just exit so I don't have to worry about this right now
+
+
+        self.logger.debug(f"Created Downloader '{self.name}'")
 
     def download(self, link: str, extra_opts: dict = {}):
         if not isinstance(extra_opts, dict):
@@ -80,9 +88,10 @@ class Downloader:
             raise
 
 
+        self.logger.debug(f"Obtaining unique download path for video {link} for dl_type={self.dl_type}")
+        dl_path = self._get_unique_dl_path(video_info.get("id"), dl_type=self.dl_type)
         try:
-            self.logger.debug(f"Obtaining unique download path for video {link} for dl_type={self.dl_type}")
-            dl_path = self._get_unique_dl_path(video_info.get("id"), dl_type=self.dl_type)
+            self._make_dl_path(dl_path)
             self.logger.debug(f"Successfully obtained unique download path for video {link} "
                               f"@ {dl_path}")
         except FileExistsError as e:
@@ -121,17 +130,19 @@ class Downloader:
             raise NotImplementedError("'remote' storage type is not yet implemented")
             return None
         elif dl_type == "local":
-            try:
-                p = (
-                        self.downloader_opts.get("local_dl_path")
-                        / Path(str(uuid4()))
-                        / Path(video_id)
-                    )
-                p.mkdir(parents=True, exist_ok=False)
-                return p
-            except FileExistsError as e:
-                self.logger.error(f"Unable to make unique download dir @ '{str(p)}'")
-                raise
+            p = (
+                    self.downloader_opts.get("local_dl_path")
+                    / Path(str(uuid4()))
+                    / Path(video_id)
+                )
+            return p
+
+    def _make_dl_path(self, dl_path: Path):
+        try:
+            dl_path.mkdir(parents=True, exit_ok=False)
+        except FileExistsError as e:
+            self.logger.error(f"Unable to make unique download dir @ '{str(p)}'")
+            raise
 
     
 
