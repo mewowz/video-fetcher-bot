0f925042aa0952031e57af35fda5b9b9aea884c3
mewowz <mewowz@lainx.org>
2026-04-18 15:12:20 -0700

    Cleaning up the download_video cog and commenting unused command args until a later implementation

--- bot/cogs/download_video.py
+++ bot/cogs/download_video.py
@@ -125,23 +125,23 @@ class DownloadVideoCog(commands.Cog):
     @_is_in_dms()
     @app_commands.describe(
         url="The url to the video you wish to download",
-        list_formats="Returns the list of available formats to download",
-        desired_filetype="The desired final video format (typically mp4 or webm)",
-        audio_format_id="The number denoting which audio format you wish to download",
-        video_format_id="The number denoting which video format you wish to download",
-        merge_formats="Whether or not you wish to receive one merged video or two separate files (audio & video)",
-        upload_to_discord="Whether or not to upload directly to discord **NOTE**: files WILL be 10MB or less; quality may suffer"
+        #list_formats="Returns the list of available formats to download",
+        #desired_filetype="The desired final video format (typically mp4 or webm)",
+        #audio_format_id="The number denoting which audio format you wish to download",
+        #video_format_id="The number denoting which video format you wish to download",
+        #merge_formats="Whether or not you wish to receive one merged video or two separate files (audio & video)",
+        #upload_to_discord="Whether or not to upload directly to discord **NOTE**: files WILL be 10MB or less; quality may suffer"
     )
     async def dl(
         self, 
         interaction: discord.Interaction, 
         url: str, 
-        list_formats: bool = False,
-        desired_filetype: str = None,
-        audio_format_id: int = None,
-        video_format_id: int = None,
-        merge_formats: bool = True,
-        upload_to_discord: bool = None,
+        #list_formats: bool = False,
+        #desired_filetype: str = None,
+        #audio_format_id: int = None,
+        #video_format_id: int = None,
+        #merge_formats: bool = True,
+        #upload_to_discord: bool = None,
     ):
         self.logger.info(
                 f"Receieved request from {interaction.user.id} "
@@ -160,24 +160,32 @@ class DownloadVideoCog(commands.Cog):
 
         job_id = uuid.uuid4().hex  # replace w/ ULID later
 
-        req = DownloadRequest(url, "auto", desired_filetype, False)
+        #req = DownloadRequest(url, "auto", desired_filetype, False)
+        req = DownloadRequest(url)
         policy = JobPolicy()
 
         payload = self._create_job_dict(
-            interaction, job_id, req, policy, list_formats, 
-            audio_format_id, video_format_id, merge_formats,
-            upload_to_discord
+            interaction, 
+            job_id, 
+            req, 
+            policy, 
+            #list_formats, 
+            #audio_format_id, 
+            #video_format_id, 
+            #merge_formats,
+            #upload_to_discord
+            None, None, None, None, None
         )
         
         self.logger.debug(
-                f"[dl] Pushing job to '{NEW_JOBS_QUEUE}' on valkey server "
-                f"with payload {json.dumps(payload)}"
+            f"Pushing job to '{NEW_JOBS_QUEUE}' on valkey server "
+            f"with payload {json.dumps(payload)}"
         )
 
         # Store job for workers to handle
         await self.redis.lpush(NEW_JOBS_QUEUE, json.dumps(payload))
 
-        self.logger.info("[dl] Successfully pushed job to queue. Returning")
+        self.logger.info(f"Successfully pushed job {payload['job_id']} to queue. Returning")
 
 
     @dl.error
