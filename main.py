7ac6dd03ec0a0851b3ea020a8445bfbe91115a33
mewowz <mewowz@lainx.org>
2026-04-30 21:36:20 -0700

    Updated main.py to support properly running each module of the worker pipeline

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 import asyncio
@@ -8,32 +9,28 @@ from bot.bot import bot, init_bot
 from utils.config import LOG_DIR, LOGFILE_NAME
 from worker.worker_pool import WorkerPool
 from worker.uploader import UploaderPool
+from worker.postprocessor_pool import PostProcessorPool
 
-def basic_stdout_logger():
-    logging.basicConfig(
-            level=logging.DEBUG,
-            format="[%(asctime)s] [%(name)s.%(funcName)s] %(levelname)s: %(message)s",
-            stream=sys.stdout
-    )
-    logging.getLogger("discord").setLevel(logging.WARNING)
+AVAILABLE_MODULES = ["bot", "downloader", "postprocessor", "uploader"]
+modules_to_run = []
 
-def setup_logger():
-    Path(LOG_DIR).mkdir(exist_ok=True)
-
+def setup_logger(log_level: int, yes_log_file: bool, log_stdout: bool):
+    root = logging.getLogger()
+    root.setLevel(log_level)
     fmt = logging.Formatter("[%(asctime)s] [%(name)s.%(funcName)s] %(levelname)s: %(message)s",)
 
-    file_handler = logging.FileHandler(Path(LOG_DIR) / Path(LOGFILE_NAME))
-    file_handler.setFormatter(fmt)
-    file_handler.setLevel(logging.DEBUG)
+    if yes_log_file:
+        Path(LOG_DIR).mkdir(exist_ok=True)
+        file_handler = logging.FileHandler(Path(LOG_DIR) / Path(LOGFILE_NAME))
+        file_handler.setFormatter(fmt)
+        file_handler.setLevel(log_level)
+        root.addHandler(file_handler)
 
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(fmt)
-    stream_handler.setLevel(logging.DEBUG)
-
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    root.addHandler(file_handler)
-    root.addHandler(stream_handler)
+    if log_stdout:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(fmt)
+        stream_handler.setLevel(log_level)
+        root.addHandler(stream_handler)
 
     logging.getLogger("discord").setLevel(logging.WARNING)
     logging.getLogger("discord.http").setLevel(logging.WARNING)
@@ -42,29 +39,122 @@ def setup_logger():
     logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="video-fetcher-bot",
+        description="video-fetcher: a program for users to download "
+                    "their favorite videos and have them available " 
+                    "download on discord"
+    )
+    
+    parser.add_argument(
+        "--modules",
+        nargs="+",
+        choices=AVAILABLE_MODULES + ["all"],
+        default=["all"],
+        metavar="MODULE",
+        help=f"Modules to run. Choices: {', '.join(AVAILABLE_MODULES + ['all'])}. Default: all"
+    )
+
+    parser.add_argument(
+        "-v", "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity level. -v for INFO, -vv for DEBUG. Default: WARNING"
+    )
+
+    parser.add_argument(
+        "--log-stdout",
+        action="store_true",
+        default=False,
+        help="Enable logging to stdout. Default: disabled"
+    )
+
+    parser.add_argument(
+        "--no-log-file",
+        action="store_true",
+        default=False,
+        help="Disable logging to file. Default: enabled"
+    )
+
+    parser.add_argument(
+        "--daemon",
+        action="store_true",
+        default=False,
+        help="Run as a daemon process"
+    )
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        default=None,
+        metavar="PATH",
+        help="Path to an alternate config file"
+    )
+
+    return parser.parse_args()
+
+def handle_log_args(args: argparse.Namespace):
+    LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
+    verbosity_level = LOG_LEVELS[min([args.verbose, 2])]
+
+    yes_log_file = not args.no_log_file
+    log_stdout = args.log_stdout
+
+    setup_logger(verbosity_level, yes_log_file, log_stdout)
+
+def handle_module_args(args: argparse.Namespace):
+    global modules_to_run
+    modules = args.modules
+    if "all" in modules:
+        modules_to_run = AVAILABLE_MODULES
+        return
+    else:
+        modules_to_run = list(modules)
+    
+def handle_daemon_arg(args: argparse.Namespace):
+    ...
+
+def handle_config_arg(args: argparse.Namespace):
+    ...
+
+def handle_args(args: argparse.Namespace):
+    handle_log_args(args)
+    handle_module_args(args)
+    handle_daemon_arg(args)
+    handle_config_arg(args)
+
 def main():
+    if "downloader" in modules_to_run:
+        downloader_pool = WorkerPool()
+        downloader_pool.start_workers()
+
+    asyncio.run(async_jobs())
+
+async def async_jobs():
+    if "uploader" in modules_to_run:
+        uploaders = UploaderPool()
+        await uploaders.start_workers()
+
+    if "postprocessor" in modules_to_run:
+        postprocessors = PostProcessorPool()
+        await postprocessors.start_workers()
+
+    if "bot" in modules_to_run:
+        await run_bot()
+    else:
+        await asyncio.Event().wait() # keep the loop alive incase we're not running the bot
+
+
+async def run_bot():
     token = os.environ.get("DISCORD_TOKEN", "")
     if not token:
         raise EnvironmentError("No environment variable set for DISCORD_TOKEN")
-
-    downloaders = WorkerPool()
-    downloaders.start_workers()
-
-    asyncio.run(async_jobs(token))
-
-async def async_jobs(token):
-    uploaders = UploaderPool()
-    await uploaders.start_workers()
-
-    await run_bot(token)
-
-async def run_bot(token):
     await init_bot()
     async with bot:
         await bot.start(token)
 
 if __name__ == "__main__":
-    # This is just used for testing right now
-    #basic_stdout_logger()
-    setup_logger()
+    args = parse_args()
+    handle_args(args)
     main()
